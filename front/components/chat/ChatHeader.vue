d25355e11fe7dee0406bd4a75ae205255af99644
datikken <tikken23@gmail.com>
2023-04-06 22:39:05 +0300

    ports mapping

--- front/components/chat/ChatHeader.vue
+++ front/components/chat/ChatHeader.vue
@@ -3,7 +3,7 @@
     <img class="object-cover w-10 h-10 rounded-full"
       src="https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHwxfHxraXR0ZW58ZW58MHx8fHwxNjczODc5NDM1&ixlib=rb-4.0.3&q=80&w=1080"
       alt="username" />
-    <span class="block ml-2 font-bold text-gray-600">Emma</span>
+    <span class="block ml-2 font-bold text-gray-600">Test</span>
     <span class="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-3">
     </span>
   </div>
